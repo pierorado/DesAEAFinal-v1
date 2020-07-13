8b0e70b18412ded6a201a74bf6862921684cba97
bryanaponte <bryan.aponte@tecsup.edu.pe>
2020-07-13 16:29:31 -0500

    commit product

--- Web/Controllers/ProductController.cs
+++ Web/Controllers/ProductController.cs
@@ -1,1 +1,59 @@
-﻿
+﻿using Models.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Web.Proxy;
+
+namespace Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductProxy proxy = new ProductProxy();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetAll()
+        {
+            var response = Task.Run(() => proxy.GetAll());
+            return Json(response.Result.List, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult Get(int id)
+        {
+            var response = Task.Run(() => proxy.Get(id));
+            return Json(response.Result.Object, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Add(Product_Request model)
+        {
+            if (model.ProductID == 0)
+            {
+                var response = Task.Run(() => proxy.Add(model));
+                return Json(response, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                var response = Task.Run(() => proxy.Update(model));
+                return Json(response, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var response = Task.Run(() => proxy.Delete(id));
+            string message = response.Result.Message;
+            return Json(new { Message = message, JsonRequestBehavior.AllowGet });
+        }
+    }
+}
