35a4c74dc40c6daa4fd81b908be96cfbab7bc0f3
BELL <BELL@DESKTOP-0FBG5QS>
2020-07-13 16:25:53 -0500

    Commit Models

--- Domain/Models/SalesInvoce.cs
+++ Domain/Models/SalesInvoce.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Domain.Models
+{
+    public class SalesInvoce
+    {
+        [Key]
+        public int SalesInvoceID { get; set; }
+
+        public string Number { get; set; }
+
+        public bool Payed { get; set; }
+
+        public int discount { get; set; }
+
+        public string Reason { get; set; }
+
+        public DateTime CreateAt { get; set; }
+
+        public bool State { get; set; }
+
+        public int CustomerID { get; set; }
+
+        public int SellerID { get; set; }
+
+        public virtual Customer Customer { get; set; }
+        public virtual Seller Seller { get; set; }
+        public virtual ICollection<SalesInvoceDetail> SalesInvoceDetails { get; set; }
+    }
+}
--- Domain/Models/SalesInvoceDetail.cs
+++ Domain/Models/SalesInvoceDetail.cs
@@ -1,1 +1,36 @@
-﻿
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Domain.Models
+{
+    public class SalesInvoceDetail
+    {
+        public int SalesInvoceDetailID { get; set; }
+
+        public string Description { get; set; }
+
+        [DataType(DataType.Currency)]
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        public decimal Price { get; set; }
+
+        [DataType(DataType.Currency)]
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
+        public float Quantity { get; set; }
+
+        public int SalesInvoceID { get; set; }
+
+        public bool State { get; set; }
+
+        public int ProductID { get; set; }
+
+        public virtual Product Product { get; set; }
+
+        [JsonIgnore]
+        public virtual SalesInvoce SalesInvoce { get; set; }
+
+
+    }
+}
